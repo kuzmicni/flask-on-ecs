a01b22d5918fb6505ac6386bff8da6d379b84c78
Nikola Kuzmic <nikola.kuzmic945@gmail.com>
2023-02-26 14:06:15 -0500

    Regression model for predicting salary

--- myapp.py
+++ myapp.py
@@ -3,15 +3,6 @@ import pickle
 import numpy as np
 import xgboost as xgb
 
-def puppy_type(prediction):
-    if int(prediction[0]) == 0:
-        puppy = 'wiener dog'
-    elif int(prediction[0]) == 1:
-        puppy = 'german shepherd'
-    else:
-        puppy = 'something went wrong'
-    return puppy
-
 flask_app_obj = Flask(__name__)
 
 with open('model.pkl', 'rb') as f:
@@ -19,26 +10,24 @@ with open('model.pkl', 'rb') as f:
 
 @flask_app_obj.route('/')
 def hi_there():
-    return 'Hola, Amigos!'
+    return 'Hi, there!'
 
 @flask_app_obj.route("/recms", methods = ["POST"])
 def make_rec():
   if request.method == "POST":
         data = request.get_json() 
-        area = int(data["area"])
-        print(area)
-        input_value = np.array([[area]])
-        # Test API: curl -X POST http://0.0.0.0:80/recms -H 'Content-Type: application/json' -d '{"area":"200"}'
+        yrs = int(data["years"])
+        input_value = np.array([[yrs]])
+        # Test API: curl -X POST http://0.0.0.0:80/recms -H 'Content-Type: application/json' -d '{"years":"5"}'
         try: 
-            print(input_value)
-            prediction = ml_model.predict(input_value)
-            puppy_rec = puppy_type(prediction)
+            prediction = ml_model.predict(input_value)[0]
+            
             
         except Exception as e:
-            puppy_rec = None
+            prediction = None
             print("An error occurred:", e)
         
-        return {"rec_puppy":puppy_rec}
+        return {"salary":prediction}
 
 if __name__ == "__main__":
     flask_app_obj.run(host='0.0.0.0', port=80)
